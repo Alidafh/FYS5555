379c911bf729da06f0c4c2ba919561ed1fa53c81
alidafh <alidafh@gmail.com>
2020-04-29 17:29:42 +0200

    min/max information and more plotting

--- project2/code/make_plots.py
+++ project2/code/make_plots.py
@@ -3,7 +3,7 @@ from array import array
 import sys
 from os import listdir
 from os.path import isfile, join
-from datetime import datetime
+from datetime import datetime, timedelta, date
 import math
 import numpy as np
 ROOT.TH1.SetDefaultSumw2()
@@ -46,7 +46,12 @@ def plotTP(x_event, y_temp_in, y_temp_out, y_pressure, min, max, name, g):
     g_in_temp = ROOT.TGraph(len(x_event), x_event, y_temp_in)
     g_in_temp.SetTitle("POLA-0{} Temperature and Pressure [10 min time interval]".format(g))
     g_in_temp.SetLineColor(4)
-    g_in_temp.SetLineWidth(2)
+    if g == 1:
+        g_in_temp.SetLineWidth(2)
+    else:
+        g_in_temp.SetLineWidth(1)
+    g_in_temp.SetMarkerStyle(7)
+    g_in_temp.SetMarkerColor(4)
     g_in_temp.SetMinimum(min)
     g_in_temp.SetMaximum(max)
     g_in_temp.GetXaxis().SetTimeDisplay(1)
@@ -61,7 +66,12 @@ def plotTP(x_event, y_temp_in, y_temp_out, y_pressure, min, max, name, g):
     g_out_temp = ROOT.TGraph(len(x_event), x_event, y_temp_out)
     g_out_temp.SetTitle("")
     g_out_temp.SetLineColor(2)
-    g_out_temp.SetLineWidth(2)
+    if g ==1:
+        g_out_temp.SetLineWidth(2)
+    else:
+        g_out_temp.SetLineWidth(1)
+    g_out_temp.SetMarkerStyle(15)
+    g_out_temp.SetMarkerColor(2)
     g_out_temp.SetMinimum(min)
     g_out_temp.SetMaximum(max)
     g_out_temp.GetXaxis().SetTimeDisplay(1)
@@ -97,17 +107,17 @@ def plotTP(x_event, y_temp_in, y_temp_out, y_pressure, min, max, name, g):
 def plotCoord(latitude, longitude, name, g):
     C = ROOT.TCanvas(name, name, 1200, 600)
     C.SetGrid()
-    g_cord = ROOT.TGraph(len(longitude), longitude, latitude)
-    g_cord.SetMarkerStyle(20)
-    g_cord.SetMarkerColor(1)
-    g_cord.SetLineColor(1)
-    g_cord.SetLineWidth(2)
-    g_cord.SetTitle("POLA-0{} Coordinates".format(g))
-    g_cord.GetXaxis().SetTitle("Longitude")
-    g_cord.GetYaxis().SetTitle("Latitude")
-    g_cord.GetYaxis().SetLabelOffset(0.02)
-    g_cord.GetYaxis().SetTitleOffset(1.4)
-    g_cord.Draw("APL")
+    g_lr = ROOT.TGraph(len(longitude), longitude, latitude)
+    g_lr.SetMarkerStyle(33)
+    g_lr.SetMarkerColor(1)
+    g_lr.SetLineColor(4)
+    g_lr.SetLineWidth(1)
+    g_lr.SetTitle("POLA-0{} Coordinates [12 h time interval]".format(g))
+    g_lr.GetXaxis().SetTitle("Longitude")
+    g_lr.GetYaxis().SetTitle("Latitude")
+    g_lr.GetYaxis().SetLabelOffset(0.02)
+    g_lr.GetYaxis().SetTitleOffset(1.4)
+    g_lr.Draw("APL")
     C.Update()
     C.Print("../figures/POLA0{}_lat_long.pdf]".format(g))
     return C
@@ -124,10 +134,10 @@ def plotRawL(time, rawrate, L, name, g):
     pad1.cd()
     g_l = ROOT.TGraph(len(time), time, L)
     g_l.SetTitle("POLA-0{} {} and Rate over time [12 h time interval]".format(g,name))
-    g_l.SetMarkerStyle(20)
+    g_l.SetMarkerStyle(33)
     g_l.SetMarkerColor(4)
     g_l.SetLineColor(4)
-    g_l.SetLineWidth(2)
+    g_l.SetLineWidth(1)
     g_l.GetXaxis().SetTimeDisplay(1)
     g_l.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M}");
     g_l.GetXaxis().SetLabelOffset(0.03)
@@ -138,10 +148,10 @@ def plotRawL(time, rawrate, L, name, g):
     pad2.cd()
     g_raw = ROOT.TGraph(len(time), time, rawrate)
     g_raw.SetTitle("")
-    g_raw.SetMarkerStyle(20)
+    g_raw.SetMarkerStyle(33)
     g_raw.SetMarkerColor(1)
     g_raw.SetLineColor(1)
-    g_raw.SetLineWidth(2)
+    g_raw.SetLineWidth(1)
     g_raw.GetXaxis().SetTimeDisplay(1)
     g_raw.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M}");
     g_raw.GetXaxis().SetLabelOffset(0.03)
@@ -153,7 +163,7 @@ def plotRawL(time, rawrate, L, name, g):
     legend = ROOT.TLegend(0.72,0.13,0.82,0.23)
     legend.SetFillStyle(0)
     legend.SetBorderSize(0)
-    legend.AddEntry(g_raw,"Rawrate","lp")
+    legend.AddEntry(g_raw,"Rawrate","p")
     legend.AddEntry(g_l, "{}".format(name), "lp")
     legend.Draw()
     C.Update()
@@ -164,7 +174,7 @@ def plotLTime(time, L, name):
     C = ROOT.TCanvas(name, name, 1200, 600)
     C.SetGrid()
     g_l = ROOT.TGraph(len(time), time, L)
-    g_l.SetMarkerStyle(20)
+    g_l.SetMarkerStyle(33)
     g_l.SetMarkerColor(1)
     #g_l.SetLineColor(1)
     #g_l.SetLineWidth(2)
@@ -190,10 +200,11 @@ def plotLatLong(time, lat, long, name, g):
     pad1.cd()
     g_long = ROOT.TGraph(len(time), time, long)
     g_long.SetTitle("POLA-01 Latitude and Longitude over time [12 h time interval]")
-    g_long.SetMarkerStyle(20)
+    g_long.SetMarkerStyle(33)
     g_long.SetMarkerColor(4)
+    #g_long.SetLineStyle(8)
     g_long.SetLineColor(4)
-    g_long.SetLineWidth(2)
+    g_long.SetLineWidth(1)
     g_long.GetXaxis().SetTimeDisplay(1)
     g_long.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M}");
     g_long.GetXaxis().SetLabelOffset(0.03)
@@ -204,10 +215,11 @@ def plotLatLong(time, lat, long, name, g):
     pad2.cd()
     g_lat = ROOT.TGraph(len(time), time, lat)
     g_lat.SetTitle("")
-    g_lat.SetMarkerStyle(20)
+    g_lat.SetMarkerStyle(33)
     g_lat.SetMarkerColor(1)
     g_lat.SetLineColor(1)
-    g_lat.SetLineWidth(2)
+    #g_lat.SetLineStyle(8)
+    g_lat.SetLineWidth(1)
     g_lat.GetXaxis().SetTimeDisplay(1)
     g_lat.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M}");
     g_lat.GetXaxis().SetLabelOffset(0.03)
@@ -233,32 +245,34 @@ def plotRaw3(x1,x2,x3,y1,y2,y3,name, title):
     mg = ROOT.TMultiGraph("mg_{}".format(name),"mg_{}".format(name))
 
     g_RawRate1 = ROOT.TGraph(len(x1), x1, y1)
-    g_RawRate1.SetMarkerStyle(20)
+    g_RawRate1.SetMarkerStyle(33)
     g_RawRate1.SetMarkerColor(1)
-    g_RawRate1.SetLineWidth(2)
-    g_RawRate1.SetLineColor(1)
+    #g_RawRate1.SetLineWidth(2)
+    #g_RawRate1.SetLineColor(1)
     g_RawRate1.SetTitle("POLA-01")
 
     g_RawRate2 = ROOT.TGraph(len(x2), x2, y2)
-    g_RawRate2.SetMarkerStyle(20)
+    g_RawRate2.SetMarkerStyle(33)
     g_RawRate2.SetMarkerColor(2)
-    g_RawRate2.SetLineWidth(2)
-    g_RawRate2.SetLineColor(2)
+    #g_RawRate2.SetLineWidth(2)
+    #g_RawRate2.SetLineColor(2)
     g_RawRate2.SetTitle("POLA-02")
 
     g_RawRate3 = ROOT.TGraph(len(x3), x3, y3)
-    g_RawRate3.SetMarkerStyle(20)
+    g_RawRate3.SetMarkerStyle(33)
     g_RawRate3.SetMarkerColor(4)
-    g_RawRate3.SetLineWidth(2)
-    g_RawRate3.SetLineColor(4)
+    #g_RawRate3.SetLineWidth(2)
+    #g_RawRate3.SetLineColor(4)
     g_RawRate3.SetTitle("POLA-03")
 
     mg.Add(g_RawRate1)
     mg.Add(g_RawRate2)
     mg.Add(g_RawRate3)
-    mg.Draw("APL")
-    mg.SetTitle("{} rate over time for all detectors [12 hour time interval]".format(title))
+    mg.Draw("AP")
+    mg.SetTitle("{} rate over time for all detectors [12 h time interval]".format(title))
     mg.GetXaxis().SetTimeDisplay(1)
+    mg.SetMinimum(5)
+    mg.SetMaximum(40)
     mg.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M}");
     mg.GetXaxis().SetLabelOffset(0.03)
     mg.GetYaxis().SetTitle("Rate [Hz]")
@@ -274,15 +288,15 @@ def plotRaw3(x1,x2,x3,y1,y2,y3,name, title):
 def raw_pressure_fit(x_pres, y_raw, min, max, p_ref, alpha, name, g):
     #   Input: x_pres, y_raw, min, max, p_ref, name, g
     #   Output: C, corrected y_rawrate
-    func = ROOT.TF1("func", "exp([0] + [1]*x)", 0, 10)
+    func = ROOT.TF1("func", "[0] + [1]*x", 2900, 6100)
     C = ROOT.TCanvas(name, name, 1200, 600)
     C.SetGrid()
     g_raw = ROOT.TGraph(len(x_pres), x_pres, y_raw)
-    g_raw.SetMarkerStyle(20)
+    g_raw.SetMarkerStyle(33)
     g_raw.SetMarkerColor(1)
     g_raw.SetLineColor(1)
     g_raw.SetLineWidth(2)
-    g_raw.SetTitle("POLA-0{} Raw rate vs pressure".format(g))
+    g_raw.SetTitle("POLA-0{} Raw rate vs pressure [12 h time interval]".format(g))
     g_raw.GetYaxis().SetTitle("Rate [Hz]")
     g_raw.GetXaxis().SetTitle("Pressure [mbar]")
     g_raw.SetMinimum(min)
@@ -294,90 +308,121 @@ def raw_pressure_fit(x_pres, y_raw, min, max, p_ref, alpha, name, g):
     legend.SetFillStyle(0)
     legend.SetBorderSize(0)
     legend.AddEntry(g_raw,"Raw rate")
-    legend.AddEntry(func,"Exponential fit: f(x)=exp[p0 + p1 x]")
+    legend.AddEntry(func,"Fit to data")
     legend.Draw()
     C.Update()
     #C.Draw()
-    C.Print("../figures/POLA0{}_rate_pressure_fit.pdf]".format(g))
+    C.Print("../figures/fixed_POLA0{}_rate_pressure_fit.pdf]".format(g))
+    h = fit.Get().Parameter(0)
     b = fit.Get().Parameter(1)
+    db = fit.Get().ParError(1)
+    avg = np.average(y_raw)
+    std = np.std(y_raw)
+    print (avg)
+    beta = b/avg
+    dbeta = beta*np.sqrt((db/b)**2 + (std/avg)**2)
+    print("beta{} = ".format(g), beta*100)
+    print("dbeta{} = ".format(g), dbeta*100)
     cor = array('d')
-    for i in range(len(x_pres)):
-        f = np.exp(alpha + b*(x_pres[i] - p_ref))
-        n = f*y_raw[i]
-        cor.append((f*y_raw[i]))
-    return C, cor, b
+    for i in range(len(y_raw)):
+        f = y_raw[i]*(1 - beta*(x_pres[i] - p_ref))
+        cor.append(f)
+    return C, cor, beta, dbeta
 
 def raw_temp_fit(x_temp, y_raw, min, max, name, g):
     #   Input: x_temp, y_raw, min, max, p_ref, name, g
     #   Output: C, corrected y_rawrate
-    func = ROOT.TF1("func", "exp([0] + [1]*x)", 0, 10)
+    #func = ROOT.TF1("func", "exp([0] + [1]*x)", 0, 10)
     C = ROOT.TCanvas(name, name, 1200, 600)
     C.SetGrid()
     g_tmp = ROOT.TGraph(len(x_temp), x_temp, y_raw)
-    g_tmp.SetMarkerStyle(20)
+    g_tmp.SetMarkerStyle(33)
     g_tmp.SetMarkerColor(1)
     g_tmp.SetLineColor(1)
     g_tmp.SetLineWidth(2)
-    g_tmp.SetTitle("POLA-0{} Raw rate vs temperature".format(g))
+    g_tmp.SetTitle("POLA-0{} Raw rate vs temperature [12 h time interval]".format(g))
     g_tmp.GetYaxis().SetTitle("Rate [Hz]")
     g_tmp.GetXaxis().SetTitle("Temperature [C]")
     g_tmp.SetMinimum(min)
     g_tmp.SetMaximum(max)
     g_tmp.Draw("AP")
-    fit = g_tmp.Fit("func", "S")
-    ROOT.gStyle.SetOptFit(111)
+    b = 0
+    #ROOT.gStyle.SetOptFit(111)
     #legend = C.BuildLegend()
-    legend = ROOT.TLegend(0.12,0.75,0.4,0.85)
-    legend.SetFillStyle(0)
-    legend.SetBorderSize(0)
-    legend.AddEntry(g_tmp,"Raw rate")
-    legend.AddEntry(func,"Exponential fit: f(x)=exp[p0 + p1 x]")
-    legend.Draw()
+    #legend = ROOT.TLegend(0.12,0.75,0.4,0.85)
+    #legend.SetFillStyle(0)
+    #legend.SetBorderSize(0)
+    #legend.AddEntry(g_tmp,"Raw rate")
+    #legend.AddEntry(func,"Fit to data")
+    #legend.Draw()
     C.Update()
     #C.Draw()
-    C.Print("../figures/POLA0{}_rate_temp_fit.pdf]".format(g))
-    a = fit.Get().Parameter(0)
-    b = fit.Get().Parameter(1)
+    C.Print("../figures/fixed_POLA0{}_rate_temp_fit.pdf]".format(g))
+    #a = fit.Get().Parameter(0)
+    #b = fit.Get().Parameter(1)
     return C, b
 
-def plotRaw1(xc, yc, x1, y1, alpha, beta, name, g):
+def plotRaw1(xc, yc, x1, y1, min, max, alpha, beta, dbeta, name, g):
         C = ROOT.TCanvas(name, name, 1200, 600)
         C.SetGrid()
         mg = ROOT.TMultiGraph("mg_{}".format(name),"mg_{}".format(name))
 
         g_RawRate2 = ROOT.TGraph(len(x1), x1, y1)
-        g_RawRate2.SetMarkerStyle(20)
+        g_RawRate2.SetMarkerStyle(33)
         g_RawRate2.SetMarkerColor(4)
-        g_RawRate2.SetLineWidth(2)
+        g_RawRate2.SetLineWidth(1)
         g_RawRate2.SetLineColor(4)
+        #g_RawRate2.SetMinimum(min)
+        #g_RawRate2.SetMaximum(max)
         g_RawRate2.SetTitle("Raw rate")
 
         g_RawRate1 = ROOT.TGraph(len(xc), xc, yc)
-        g_RawRate1.SetMarkerStyle(20)
+        g_RawRate1.SetMarkerStyle(33)
         g_RawRate1.SetMarkerColor(1)
-        g_RawRate1.SetLineWidth(2)
+        g_RawRate1.SetLineWidth(1)
         g_RawRate1.SetLineColor(1)
+        #g_RawRate1.SetMinimum(min)
+        #g_RawRate1.SetMaximum(max)
         g_RawRate1.SetTitle("Corrected rate")
 
         mg.Add(g_RawRate2)
         mg.Add(g_RawRate1)
-        mg.Draw("APL")
-        mg.SetTitle("POLA-0{} Rate, #alpha={:f} and #beta={:f}, [12 hour time interval]".format(g, alpha, beta))
+        mg.Draw("AP")
+        mg.SetTitle("POLA-0{} Rate, #alpha={:.1f} %/C and #beta={:.3f}#pm{:.3f} %/mbar, [12 h time interval]".format(g, alpha, beta*100, np.abs(dbeta)*100))
         mg.GetXaxis().SetTimeDisplay(1)
         mg.GetXaxis().SetTimeFormat("#splitline{%d/%m/%y}{%H:%M}");
         mg.GetXaxis().SetLabelOffset(0.03)
         mg.GetYaxis().SetTitle("Rate [Hz]")
+        mg.SetMinimum(min)
+        mg.SetMaximum(max)
         #mg.GetYaxis().SetLabelOffset(0.01)
         #mg.GetYaxis().SetTitleOffset(1.1);
-        if g == 1:
-            legend = C.BuildLegend(0.73,0.3,0.9,0.44)
-        else:
-            legend = C.BuildLegend(0.12,0.75,0.4,0.85)
+        legend = C.BuildLegend(0.12,0.75,0.4,0.85)
         legend.SetFillStyle(0)
         legend.SetBorderSize(0)
         C.Update()
-        C.Print("../figures/POLA0{}_corrected_rate.pdf]".format(g))
+        C.Print("../figures/fixed_POLA0{}_corrected_rate.pdf]".format(g))
         return C
+
+def plotLraw(x_L, y_raw, name, g):
+    C = ROOT.TCanvas("{}{}".format(name,g), "{}{}".format(name,g), 1200, 600)
+    C.SetGrid()
+    g_lr = ROOT.TGraph(len(x_L), x_L, y_raw)
+    g_lr.SetMarkerStyle(33)
+    g_lr.SetMarkerColor(1)
+    g_lr.SetLineColor(4)
+    g_lr.SetMinimum(25)
+    g_lr.SetMaximum(38)
+    g_lr.SetLineWidth(1)
+    g_lr.SetTitle("POLA-0{} Rate over {} [12 h time interval]".format(g, name))
+    g_lr.GetXaxis().SetTitle("{}".format(name))
+    g_lr.GetYaxis().SetTitle("Rate [Hz]")
+    g_lr.GetYaxis().SetLabelOffset(0.02)
+    g_lr.GetYaxis().SetTitleOffset(1.4)
+    g_lr.Draw("AP")
+    C.Update()
+    C.Print("../figures/POLA0{}_raw_coordinate_{}.pdf]".format(g,name))
+    return C
 
 #######################################
 #    Read data
@@ -413,15 +458,15 @@ x3_temp, y3_rawrate1 = readfile("03_raw_temp.txt")
 
 #   C = plotTP(x_event, y_temp_in, y_temp_out, y_pressure, min, max, name, g)
 C11 = plotTP(x1_event, y1_temp_inside, y1_temp_outside, y1_press, 15, 35, "temp_pres1", 1)
+
 C12 = plotTP(x2_event, y2_temp_inside, y2_temp_outside, y2_press, 23.5, 26, "temp_pres2", 2)
 C13 = plotTP(x3_event, y3_temp_inside, y3_temp_outside, y3_press, 29, 38, "temp_pres3", 3)
 
 # C = plotRaw3(x1,x2,x3,y1,y2,y3,name)
-CA = plotRaw3(x1_rawrate,x2_rawrate,x3_rawrate,y1_rawrate,y2_rawrate,y3_rawrate, "raw3", "Raw")
+CA = plotRaw3(x1_rawrate,x2_rawrate,x3_rawrate,y1_rawrate,y2_rawrate,y3_rawrate, "raw3_fixed", "Uncorrected")
 
 #   C = plotCoord(latitude, longitude, name, g)
 C21 = plotCoord(y1_lat, y1_long, "cord1", 1)
-
 
 #   C = plotLatLong(time, lat, long, name, g)
 C31 = plotLatLong(x1_rawrate, y1_lat, y1_long, "cord_time", 1)
@@ -431,19 +476,99 @@ C41 = plotRawL(x1_rawrate, y1_rawrate, y1_lat, "Latitude", 1)
 C42 = plotRawL(x1_rawrate, y1_rawrate, y1_long, "Longitude", 1)
 
 #   C1 = raw_temp_fit(x_temp, y_raw, min, max, p_ref, name, g)
-C71, a1 = raw_temp_fit(x1_temp, y1_rawrate1, 5, 45, "tmp_fit", 1)
-C72, a2 = raw_temp_fit(x2_temp, y2_rawrate1, 28, 38, "tmp_fit2", 2)
-C73, a3 = raw_temp_fit(x3_temp, y3_rawrate1, 26, 29, "tmp_fit3", 3)
+C71, a1 = raw_temp_fit(x1_temp, y1_rawrate, 5, 45, "tmp_fit", 1)
+C72, a2 = raw_temp_fit(x2_temp, y2_rawrate, 5, 45, "tmp_fit2", 2)
+C73, a3 = raw_temp_fit(x3_temp, y3_rawrate, 5, 45, "tmp_fit3", 3)
 
+a1 = a2 = a3 = 0
 #   C = raw_pressure_fit(x_pres, y_raw, min, max, p_ref, alpha, name, g)
-C51, y1_raw_cor,b1  = raw_pressure_fit(x1_press, y1_rawrate1, 5, 50, 1011.85, a1, "pr1", 1)
-C52, y2_raw_cor,b2 = raw_pressure_fit(x2_press, y2_rawrate1, 30, 40, 1008.53, a2, "pr2", 2)
-C53, y3_raw_cor,b3 = raw_pressure_fit(x3_press, y3_rawrate1, 27, 28.5, 985.87, a3, "pr3", 3)
+C51, y1_raw_cor, b1, db1 = raw_pressure_fit(x1_press, y1_rawrate, 10, 45, 1011.85, a1, "pr1", 1)
+C52, y2_raw_cor, b2, db2 = raw_pressure_fit(x2_press, y2_rawrate, 10, 45, 1008.53, a2, "pr2", 2)
+C53, y3_raw_cor, b3, db3 = raw_pressure_fit(x3_press, y3_rawrate, 10, 45,  985.87, a3, "pr3", 3)
 
 #   C = plotRaw1(xc, yc, x1, y1, alpha, beta, name, g)
-C61 = plotRaw1(x1_rawrate, y1_raw_cor, x1_rawrate, y1_rawrate1, a1, b1, "c_raw", 1)
-C62 = plotRaw1(x2_rawrate, y2_raw_cor, x2_rawrate, y2_rawrate1, a2, b2,"c_raw2", 2)
-C63 = plotRaw1(x3_rawrate, y3_raw_cor, x3_rawrate, y3_rawrate1, a3, b3,"c_raw3", 3)
+C61 = plotRaw1(x1_rawrate, y1_raw_cor, x1_rawrate, y1_rawrate, 5,45, 0, b1, db1,"c_raw", 1)
+C62 = plotRaw1(x2_rawrate, y2_raw_cor, x2_rawrate, y2_rawrate, 5,45, 0, b2, db2, "c_raw2", 2)
+C63 = plotRaw1(x3_rawrate, y3_raw_cor, x3_rawrate, y3_rawrate, 5,45, 0, b3, db3, "c_raw3", 3)
 
 #C = plotRaw3(x1,x2,x3,y1,y2,y3,name)
-C9 = plotRaw3(x1_rawrate,x2_rawrate,x3_rawrate,y1_raw_cor,y2_raw_cor,y3_raw_cor,"corr3","Corrected")
+C9 = plotRaw3(x1_rawrate,x2_rawrate,x3_rawrate,y1_raw_cor,y2_raw_cor,y3_raw_cor,"fixed_corr3","Corrected")
+
+
+#######################################
+
+#######################################
+
+def make_arr_max_min(x, y, title):
+    d = datetime(2007, 1, 1, 0, 0, 0)
+    xn = np.array(x)
+    yn = np.array(y)
+    y_avg = np.average(yn)
+    #y_var = np.var(yn, dtype=np.float64)
+    ymax = yn.max()
+    ymin = yn.min()
+    y_var = np.abs(ymax - ymin)
+    xmax = xn.max()
+    xmin = xn.min()
+    j = 0
+    h = 0
+    for i in range(len(x)):
+        if yn[i]  == ymax:
+            j = i
+            break
+        elif yn[i] == ymin:
+            h = i
+        else:
+            continue
+    xmax = xn[int(j)]
+    xmin = xn[int(h)]
+    d1 = timedelta(seconds = xmax)
+    d2 = timedelta(seconds = xmin)
+    d3 = d + d1
+    d4 = d + d2
+    #print ("{} & & Minimum & Maximum & Average & Difference".format(title))
+    print ("{} & {:.2f}({}) & {:.2f}({}) & {:.2f} & {:.2f} ".format(title, ymax, d3.date(), ymin, d4.date(),y_avg, y_var))
+
+print ("------------------")
+print ("POLA-01")
+print ("------------------")
+make_arr_max_min(x1_event, y1_temp_inside, "Indoor temperature")
+make_arr_max_min(x1_event, y1_temp_outside, "Outdoor temperature")
+make_arr_max_min(x1_event, y1_press, "Pressure")
+print ("------------------")
+print ("POLA-02")
+print ("------------------")
+make_arr_max_min(x2_event, y2_temp_inside, "Indoor Temperature")
+make_arr_max_min(x2_event, y2_temp_outside, "Outdoor Temperature")
+make_arr_max_min(x2_event, y2_press, "Pressure")
+print ("------------------")
+print ("POLA-03")
+print ("------------------")
+make_arr_max_min(x3_event, y3_temp_inside, "Indoor Temperature")
+make_arr_max_min(x3_event, y3_temp_outside, "Outdoor Temperature")
+make_arr_max_min(x3_event, y3_press, "Pressure")
+
+def make_arr_rawmin(x, y, title):
+    d = datetime(2007, 1, 1, 0, 0, 0)
+    xn = np.array(x)
+    yn = np.array(y)
+    ymax = yn.max()
+    ymin = yn.min()
+    j = []
+    y1 = []
+    xmax = []
+    for i in range(len(x)):
+        if yn[i]  < 15:
+            j.append(i)
+            y1.append(yn[i])
+            xmax.append(xn[i])
+        else:
+            continue
+    print (j)
+    d1 = timedelta(seconds = xmax[0])
+    d2 = timedelta(seconds = xmax[1])
+    d3 = d + d1
+    d4 = d + d2
+    print ("{} & {:.2f}({}) & {:.2f}({})".format(title, y1[0], d3.date(), y1[1], d4.date()))
+
+make_arr_rawmin(x1_rawrate, y1_rawrate, "rawrate")
