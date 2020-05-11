38812327c22e448689c4e04f1c405c196fa47ef4
alidafh <alidafh@gmail.com>
2020-05-11 20:58:22 +0200

    sript to run program with process options

--- project3/code/run_analysis.sh
+++ project3/code/run_analysis.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-
 echo "0 - Data"
 echo "1 - MC"
 echo "2 - Both"
 
 read type
 
-if [ $type -eq 0 ]
-then
+if [ ! -d output/data ]; then
+  mkdir -p output/data;
+  echo "created output/datafiles"
+fi
+if [ ! -d output/mc ]; then
+  mkdir -p output/mc;
+  echo "created output/mc"
+fi
+if [ ! -d output/figures ]; then
+  mkdir -p output/figures;
+  echo "created output/figures"
+fi
+
+if [ $type -eq 0 ]; then
    echo "Analysis on real data started..."
    python run_analysis.py 0 $type
    python run_analysis.py 1 $type
@@ -15,8 +26,7 @@ then
    python run_analysis.py 3 $type
 fi
 
-if [ $type -eq 1 ]
-then
+if [ $type -eq 1 ]; then
    echo "Analysis on Monte Carlo simulations started..."
    python run_analysis.py 0 $type
    python run_analysis.py 1 $type
@@ -25,8 +35,7 @@ then
    python run_analysis.py 4 $type
 fi
 
-if [ $type -eq 2 ]
-then
+if [ $type -eq 2 ]; then
    echo "Analysis on both Data and Monte Carlo started..."
    echo "this may take some time, you should go get some coffee while you wait.."
    python run_analysis.py 0 0
@@ -40,4 +49,4 @@ then
    python run_analysis.py 4 1
 fi
 
-pkill -f run_analysis.py
+#pkill -f run_analysis.py
